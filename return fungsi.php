10c6b98cc35ccf4817ec8b08a2858fd3a955231a
wwidi <wwidi4087@gmail.com>
2025-10-07 14:03:33 +0700

    return fungsi.php

--- return fungsi.php
+++ return fungsi.php
@@ -42,3 +42,14 @@ $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 perkenalan($saya, $ucapanSalam);
 ?>
+
+<?php
+// membuat fungsi 
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah " . hitungUmur(2006, 2025) . " tahun";
+
+?>
