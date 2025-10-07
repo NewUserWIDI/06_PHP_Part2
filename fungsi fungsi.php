f76e0f1b8e76bd1cc15a2f4f13a846cd5afcdce5
wwidi <wwidi4087@gmail.com>
2025-10-07 14:05:41 +0700

    fungsi fungsi.php

--- fungsi fungsi.php
+++ fungsi fungsi.php
@@ -53,3 +53,22 @@ function hitungUmur($thn_lahir, $thn_sekarang){
 echo "Umur saya adalah " . hitungUmur(2006, 2025) . " tahun";
 
 ?>
+<?php
+//membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>";
+    //memanggil fungsi lain
+    echo "Saya berusia " . hitungUmur(1988, 2023) . " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+//memanggil fungsi perkenalan
+perkenalan("Elok");
+
+?>
