4bd0e1636f0c334bbcb2d405da2d24ca08310e89
wwidi <wwidi4087@gmail.com>
2025-10-07 13:51:31 +0700

    fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaiku, ";
+    echo "Perkenalkan, nama saya Widi<br/>"; 
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan(); 
+perkenalan();
+?>
