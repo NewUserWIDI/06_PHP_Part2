e37bd683398568a4090c8126a31e13c97109333a
wwidi <wwidi4087@gmail.com>
2025-10-07 14:00:36 +0700

    nilai default fungsi.php

--- nilai default fungsi.php
+++ nilai default fungsi.php
@@ -20,6 +20,24 @@ perkenalan("Hamdana", "Hallo");
 
 echo "<hr>"; 
 
+<?php
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan("Hamdana", "Hallo");
+
+echo "<hr>"; 
+
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya);
+?>
+
 $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 perkenalan($saya, $ucapanSalam);
